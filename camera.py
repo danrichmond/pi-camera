42cae299b6f9c3943e4e398a475fe76fa4c68333
Dan Richmond <danrichmond@comcast.net>
2017-09-07 02:36:16 +0000

    Comment

--- camera.py
+++ camera.py
@@ -8,19 +8,21 @@ action = input("Enter 1 for photo or 2 for video: ")
 camera.rotation = 180
 
 if action == "1":
-  camera.start_preview() # Is the preview necessary?
+  camera.start_preview() # Previews only temporarily necessary
   sleep(5)
   camera.capture('/home/pi/Desktop/folder/images/image.jpg')
-  camera.stop_preview() # Is the preview necessary?
+  camera.stop_preview()
 elif action == "2":
   duration = input("Video length is seconds: ")
   camera.brightness = 55
-  camera.start_preview() # Is the preview necessary?
+  camera.start_preview()
   camera.start_recording('/home/pi/Desktop/folder/videos/video.h264')
-  sleep(int(duration) - 1) # Record for duration user gave
+  sleep(int(duration) - 1)
   camera.stop_recording()
-  camera.stop_preview() # Is the preview necessary?
+  camera.stop_preview()
 else:
   print("You did not enter 1 or 2")
+
+# Insert API code here, use picture from above.
 
 print("Done execution.")
